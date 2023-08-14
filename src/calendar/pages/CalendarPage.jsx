e4155cdc0cc931f982423bfbb82f6c93cd2e2fd6
Cristianps1988 <cristianps_1@hotmail.com>
2023-08-13 19:50:27 -0500

    importin the calendar package

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,6 +1,38 @@
+import { Calendar } from 'react-big-calendar'
+import { addHours } from 'date-fns'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { localizer, getMessagesES } from '../../helpers'
+
+import { Navbar } from "../components/Navbar"
+
+const myEventsList = [{
+    title: 'Cumpleaños de juan',
+    start: new Date(),
+    notes: 'Hay que comprar el pastel',
+    end: addHours(new Date(), 1),
+
+}]
 
 export const CalendarPage = () => {
+
+    const eventPropGetter = (event, start, end, isSelected) => {
+        console.log({ event, start, end, isSelected })
+    }
+
     return (
-        <div>CalendarPage</div>
+        <>
+            <Navbar />
+
+            <Calendar
+                culture='es'
+                localizer={localizer}
+                events={myEventsList}
+                startAccessor="start"
+                endAccessor="end"
+                style={{ height: 'calc(100vh - 80px)' }}
+                messages={getMessagesES()}
+                eventPropGetter={eventPropGetter}
+            />
+        </>
     )
 }
